4fe7c5cfcbe16b97677756b3dd0b39404f783d8a
snirpy <76662720+snirpy@users.noreply.github.com>
2024-10-14 21:29:24 +0200

    Delete Ex1_conversion_float_to_virgule_Methode1.py

--- Ex1_conversion_float_to_virgule_Methode1.py
+++
@@ -1,8 +0,0 @@
-saisi_str = input("Sasir un flottant: ")
-saisi_float = float(saisi_str)
-print("Le nombre flottant saisi est: ",saisi_float)
-
-entiere, decimale = saisi_str.split(".")
-
-monNombe =",".join([entiere,decimale])
-print("mon nombre est ",monNombe)
